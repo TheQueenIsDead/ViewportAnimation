dfbba249274807389a253e57ce3d81d76ab897cc
David King <dki27@uclive.ac.nz>
2017-08-17 22:44:36 +1200

    Added a working timer to shift the viewport to the second side of the image

--- src/sample/firstController.java
+++ src/sample/firstController.java
@@ -1,11 +1,6 @@
 package sample;
 
-import javafx.animation.AnimationTimer;
-import javafx.animation.PauseTransition;
 import javafx.animation.Transition;
-import javafx.animation.TranslateTransition;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
@@ -14,9 +9,10 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class firstController implements Initializable{
 
@@ -28,7 +24,9 @@ public class firstController implements Initializable{
 
     private double width = 600;
     private double height = 400;
-    double target = 700;
+    double start = 0;
+    double target = 600;
+    Rectangle2D rect = new Rectangle2D(0, 0, 600, 400);
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -39,13 +37,20 @@ public class firstController implements Initializable{
     private void animateViewport() {
         //Needs to slowly update the XOffset value from 0 -> 600
         Transition t = new Transition() {
+            {
+                setCycleDuration(Duration.seconds(3));
+            }
             @Override
             protected void interpolate(double frac) {
                 System.out.println(frac);
+                double offset = start + frac * (target - start);
+                rect = new Rectangle2D(offset, 0, width, height);
+                imgView.setViewport(rect);
             }
         };
-        t.setCycleCount(15);
-        t.playFromStart();
+
+        t.play();
+
     }
 
     private void initiateImage(){
@@ -53,7 +58,7 @@ public class firstController implements Initializable{
 
         //*Min x Is the one that sets the offset in the image, where it draws the width and height from (top left)
 
-        imgView.setViewport(new Rectangle2D(0, 0, width, height));
+        imgView.setViewport(new Rectangle2D(600, 0, width, height));
         imgView.setFitHeight(400);
         imgView.setFitWidth(600);
         imgView.setPreserveRatio(false);
